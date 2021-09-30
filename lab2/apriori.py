112f2775e985951bd964dedab7d34b14b71fc8f6
Ryan Hunter <rghunter@calpoly.edu>
2021-09-30 16:28:49 -0700

    work from thurs

--- lab2/apriori.py
+++ lab2/apriori.py
@@ -1,4 +1,14 @@
 import pandas
 import numpy
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--file')
+parser.add_argument('--minrs', type=int)
+args = parser.parse_args()
 
+def load_good_labels(filename):
+    good_labels_dataframe = pandas.read_csv(filename, index_col="Id")
+    print(good_labels_dataframe)
+
+load_good_labels(args.file)
