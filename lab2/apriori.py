0bf60d9058ab584d81beb11a775ff79395bf814e
Taylor Bedrosian <taylorbedrosian@gmail.com>
2021-10-05 01:33:00 -0700

    Round 1 Candidates and Round 2 Supports

--- lab2/apriori.py
+++ lab2/apriori.py
@@ -1,6 +1,7 @@
 import pandas
 import numpy
 import argparse
+import itertools
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file')
@@ -28,10 +29,41 @@ def apriori_freq_first_pass(transactions, items, minsup):
     items - list from 1-50 representing bakery items and indeces of df vector
     minsup - minsupport
     """
+    freq_sets = {}
     item_counts = {i: sum(transactions[i]) for i in transactions.columns}
     sum_all_items = sum(item_counts[i] for i in transactions.columns)
     item_rsup = {i: item_counts[i] / sum_all_items for i in transactions.columns}
     print(item_rsup)
+
+    freq_sets[1] = {'f1': round_1_candidates(items, item_rsup, minsup)}
+    k = 2
+    print(freq_sets[1])
+    while len(freq_sets[k-1]) > 0:
+        candidates = candidate_gen(freq_sets[k-1], k)
+        count_dict = {candidates[i]: 0 for i in range(0, len(candidates))}
+        for candidate in count_dict:
+            candidate_indexes = numpy.asarray(candidate)
+            for i in range(len(candidate_indexes)):
+                candidate_indexes[i] -= 1
+            for i in range(0, len(transactions)):
+                row = transactions.iloc[i, candidate_indexes]
+                if row.sum() == k:
+                    count_dict[candidate] += 1
+
+        print("DONE WITH ROUND COUNTING")
+        still_frequent = []
+        for i in count_dict:
+            print(count_dict[i] / sum_all_items)
+            if (count_dict[i] / sum_all_items) >= minsup:
+                still_frequent.append(i)
+                
+        freq_sets[k] = {'f'+str(k): still_frequent}
+        print(freq_sets[k], end=' ')
+        
+        k += 1
+        
+        
+
     for tid in transactions.columns:
         continue
         """
@@ -42,13 +74,28 @@ def apriori_freq_first_pass(transactions, items, minsup):
 
 
 def apriori_freq_itemsets(transactions, items, minsup):
-    # first pass    
+    # first pass
     pass
     
+def round_1_candidates(items, item_rsup, minsup):
+    candidates = []
+    for item in items:
+       if item_rsup[item] >= minsup:
+          candidates.append(item)
+    return candidates
+
+
+def candidate_gen(candidates, k):
+    '''Note, this solely does the join step, not pruning currently'''
+    union = itertools.combinations(candidates['f'+str((k-1))], k)
+    c = []
+    for i in union:
+        c.append(i)
+    return c
 
 if __name__ == "__main__":
     good_labels_df = load_good_labels(GOODS_FILE)
     bakery_bv_df = load_full_bv_bakery(f"1000/1000{BAKERY_OUT2}")
-    apriori_freq_first_pass(bakery_bv_df, (i for i in range(1, 51)), 0.08)
+    apriori_freq_first_pass(bakery_bv_df, (i for i in range(1, 51)), 0.026)
 
 
